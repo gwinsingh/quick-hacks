a163c180c58501a2c08c4621a1bbef16db8135f3
Gurwinder Singh <gwinsingh@gmail.com>
2018-10-23 18:30:27 +0530

    Update facebook_autodelete.js
    Auto delete messenger group chats selectively.
    -- first half commit

---
+++ facebook_autodelete.js
@@ -0,0 +1,10 @@
+
+
+setInterval(function(){
+	try{
+	document.getElementsByClassName("_3quh _30yy _2t_ _3ay_ _5ixy")[0].click()
+	}
+	catch(err){
+		//console.log('ERROR:\n',err.message);
+	}
+},300);
